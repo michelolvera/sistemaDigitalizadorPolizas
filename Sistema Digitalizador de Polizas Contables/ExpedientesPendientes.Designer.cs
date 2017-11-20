d364dfd2685aa63c524cc7050e9340b1c39331b9
alan_ <roblesvazquez@tecmor.mx>
2017-11-20 14:50:04 -0600

    Varias modificaciones en la interfaz

---
+++ Sistema Digitalizador de Polizas Contables/ExpedientesPendientes.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Sistema_Digitalizador_de_Polizas_Contables
+{
+    partial class expedientesP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(expedientesP));
+            this.dgvExpedientes = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCreacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExpedientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvExpedientes
+            // 
+            this.dgvExpedientes.AllowUserToAddRows = false;
+            this.dgvExpedientes.AllowUserToDeleteRows = false;
+            this.dgvExpedientes.AllowUserToOrderColumns = true;
+            this.dgvExpedientes.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dgvExpedientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvExpedientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNum,
+            this.colNombre,
+            this.colCategoria,
+            this.colCreacion});
+            this.dgvExpedientes.Location = new System.Drawing.Point(12, 46);
+            this.dgvExpedientes.Name = "dgvExpedientes";
+            this.dgvExpedientes.ReadOnly = true;
+            this.dgvExpedientes.Size = new System.Drawing.Size(739, 348);
+            this.dgvExpedientes.TabIndex = 0;
+            this.dgvExpedientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pendientes";
+            // 
+            // colNum
+            // 
+            this.colNum.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colNum.HeaderText = "Número. Expediente";
+            this.colNum.Name = "colNum";
+            this.colNum.ReadOnly = true;
+            // 
+            // colNombre
+            // 
+            this.colNombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colNombre.HeaderText = "Nombre Expediente";
+            this.colNombre.Name = "colNombre";
+            this.colNombre.ReadOnly = true;
+            // 
+            // colCategoria
+            // 
+            this.colCategoria.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colCategoria.HeaderText = "Categoria";
+            this.colCategoria.Name = "colCategoria";
+            this.colCategoria.ReadOnly = true;
+            // 
+            // colCreacion
+            // 
+            this.colCreacion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colCreacion.HeaderText = "Fecha de Creación";
+            this.colCreacion.Name = "colCreacion";
+            this.colCreacion.ReadOnly = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnActualizar.BackgroundImage")));
+            this.btnActualizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnActualizar.Location = new System.Drawing.Point(715, 10);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(36, 30);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // expedientesP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 416);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvExpedientes);
+            this.Name = "expedientesP";
+            this.Text = "Expedientes Pendientes de Digitalizasr";
+            this.Load += new System.EventHandler(this.expedientesP_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExpedientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvExpedientes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCreacion;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
--- Sistema Digitalizador de Polizas Contables/ExpedientesPendientes.cs
+++ Sistema Digitalizador de Polizas Contables/ExpedientesPendientes.cs
@@ -15,9 +15,24 @@ namespace Sistema_Digitalizador_de_Polizas_Contables
         public expedientesP()
         {
             InitializeComponent();
+            dgvExpedientes.Rows.Add();
+            dgvExpedientes[0, 0].Value = "1";
+            dgvExpedientes[1, 0].Value = "hola";
+            dgvExpedientes[2, 0].Value = "caca";
+            dgvExpedientes[3, 0].Value = "32-02-3017";
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void expedientesP_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
         {
 
         }
