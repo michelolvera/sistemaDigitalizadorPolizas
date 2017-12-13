bad28a4757736504502e92f03bc538b7f8c9b5b0
Michel Olvera <michelolvera@icloud.com>
2017-12-13 14:14:26 -0600

    Entidad Usuario agregada

---
+++ Entidades/UsuarioInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class UsuarioInfo
+    {
+        public string nombre { get; private set; }
+        public string apellidoP { get; private set; }
+        public string apellidoM { get; private set; }
+        public string userName { get; private set; }
+        public string userPassword { get; private set; }
+        public UsuarioInfo(string nombre, string apellidoP, string apellidoM, string userName, string userPassword)
+        {
+            this.nombre = nombre;
+            this.apellidoP = apellidoP;
+            this.apellidoM = apellidoM;
+            this.userName = userName;
+            this.userPassword = userPassword;
+        }
+
+        
+    }
+}
